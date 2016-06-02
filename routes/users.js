2988f37d45f1125dd5b4dc0801fb961c348ee14c
jayanth2527 <kjayanth50@yahoo.in>
2016-06-02 17:21:09 +0530

    added files

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
-
+var orgRelService=require('./routes/index');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.route('/managerDetails/:empId')
+.get(function(req,res,next){
+     orgRelService.getAll(function(err,result){
+       if(err) res.send(err);
+       console.log('Inside Get Promise');
+       console.log(result);
+       res.json(result);
+     });
+  });
+
+  router.route('/TeamDetails/:ProjectTitle')
+  .get(function(req,res,next){
+       orgRelService.getAll(function(err,result){
+         if(err) res.send(err);
+         console.log('Inside Get Promise');
+         console.log(result);
+         res.json(result);
+       });
+    });
+
+    router.route('/AlternativeManager/:ManagerID')
+    .get(function(req,res,next){
+         orgRelService.getAll(function(err,result){
+           if(err) res.send(err);
+           console.log('Inside Get Promise');
+           console.log(result);
+           res.json(result);
+         });
+      });
+
+      router.route('/empName/:empID')
+      .get(function(req,res,next){
+           orgRelService.getAll(function(err,result){
+             if(err) res.send(err);
+             console.log('Inside Get Promise');
+             console.log(result);
+             res.json(result);
+           });
+        });
+
+      router.route('/EmpHiearchy/:EmpID/ManagerID/ManagermanagerID')
+      .get(function(req,res,next){
+           orgRelService.getAll(function(err,result){
+             if(err) res.send(err);
+             console.log('Inside Get Promise');
+             console.log(result);
+             res.json(result);
+           });
+        });
 module.exports = router;
